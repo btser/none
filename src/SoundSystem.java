be2a32bb3aea17f0052e8c9f6f4cce7543db5f12
tulna <vthanhnhan2k6@gmail.com>
2026-05-20 20:38:50 +0700

    create repo

---
+++ src/SoundSystem.java
@@ -0,0 +1,6 @@
+public class SoundSystem {
+    public void playSound(String sound) {
+        // Code to play the specified sound
+        System.out.println("Playing sound: " + sound);
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+    public String id;
+    public String type;
+    public double x, y;
+    public double angle;
+    public Vehicle(String id, String type, double x, double y, double angle) {
+        this.id = id;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
+}
